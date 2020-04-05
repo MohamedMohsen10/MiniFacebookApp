35dcff3b422cccf054a9e2d013ed4616ef88de0b
mostafaAli122 <mostafaali122@outlook.com>
2020-04-06 00:37:29 +0200

    Adding Models

---
+++ MiniFacebook/MiniFacebook/Models/RepoClass/PostLikeRepo.cs
@@ -0,0 +1,19 @@
+﻿using MiniFacebook.Data;
+using MiniFacebook.Models.Entities;
+using MiniFacebook.Models.RepoInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniFacebook.Models.RepoClass
+{
+    public class PostLikeRepo:IPostLikeRepo
+    {
+        private readonly ApplicationDbContext context;
+        public PostLikeRepo(ApplicationDbContext _context)
+        {
+            context = _context;
+        }
+    }
+}
